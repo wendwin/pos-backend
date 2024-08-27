e23b2c59d5575b69e3dc7359e2b3fd71ddbfffb5
wendwin <wendwin12@gmail.com>
2024-08-27 14:07:23 +0700

    add item detail views

--- posproject/posapp/views.py
+++ posproject/posapp/views.py
@@ -17,3 +17,28 @@ def items_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def items_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        items = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ItemSerializer(items)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ItemSerializer(items, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        items.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
