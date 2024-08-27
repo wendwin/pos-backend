6f4b93e82a9c60ddac3f23311e6e66c17b31f727
wendwin <wendwin12@gmail.com>
2024-08-27 14:15:55 +0700

    model registration to admin view

--- posproject/posapp/admin.py
+++ posproject/posapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from posapp.models import Item, Kategori, Pesanan, PesananItem
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(Kategori)
+admin.site.register(Pesanan)
+admin.site.register(PesananItem)
