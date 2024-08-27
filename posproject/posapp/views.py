e0b2c2bcd5d45e86edaceb179f4a1c2e622a5faa
wendwin <wendwin12@gmail.com>
2024-08-27 14:06:35 +0700

    add item list and item detail views

--- posproject/posapp/views.py
+++ posproject/posapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from posapp.models import Item
+from posapp.serializers import ItemSerializer
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def items_list(request): 
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
