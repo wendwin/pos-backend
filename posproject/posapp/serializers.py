980000267c8a08642dcec5d4989b282541e695f8
wendwin <wendwin12@gmail.com>
2024-09-12 14:50:49 +0700

    add order date format in order serializer

--- posproject/posapp/serializers.py
+++ posproject/posapp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from posapp.models import Item, Kategori, Pesanan, PesananItem
+from django.utils.timezone import localtime
 
 class KategoriSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,9 +14,13 @@ class ItemSerializer(serializers.ModelSerializer):
         fields = ['id_item', 'nama_item', 'nama_kategori', 'harga', 'gambar']
 
 class PesananSerializer(serializers.ModelSerializer):
+    tgl_pesanan_format = serializers.SerializerMethodField()
     class Meta:
         model = Pesanan
-        fields = ['id_pesanan', 'tgl_pesanan', 'sub_total', 'pajak', 'total', 'status', 'customer']
+        fields = ['id_pesanan', 'tgl_pesanan_format', 'sub_total', 'pajak', 'total', 'status', 'customer']
+
+    def get_tgl_pesanan_format(self, obj):
+        return localtime(obj.tgl_pesanan).strftime('%Y-%m-%d %H:%M:%S')
 
 class PesananItemSerializer(serializers.ModelSerializer):
     class Meta:
